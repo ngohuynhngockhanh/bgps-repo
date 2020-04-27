09a69a3671fa8bf71f56382ff956d74b709789a9
Ngô Huỳnh Ngọc Khánh <ngohuynhngockhanh@gmail.com>
2020-04-27 10:46:12 +0700

    Create user_script.cpp

---
+++ user_script.cpp
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(115200);
+  pinMode(2, OUTPUT);
+}
+void loop() {
+  static unsigned long timer = 0;
+  if (millis() - timer > 1000) {
+    timer = millis();
+    digitalWrite(2, 1 - digitalRead(2));
+    Serial.println(".");
+  }
+}
